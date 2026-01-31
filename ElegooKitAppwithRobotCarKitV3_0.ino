625b75885dd42506dd76d76cd761a8c9c2cd699b
TheTechNerd777 <mail@alexander-gaiswinkler.de>
2026-01-31 19:36:28 +0100

    Update ElegooKitAppwithRobotCarKitV3_0.ino

--- ElegooKitAppwithRobotCarKitV3_0.ino
+++ ElegooKitAppwithRobotCarKitV3_0.ino
@@ -17,11 +17,16 @@
 #define LT_R !digitalRead(10)
 
 Servo head;
-int carSpeed = 230; // Wird jetzt live durch App-Balken (D2) überschrieben
+int carSpeed = 200;  // Speichert DAUERHAFT deine App-Balken Einstellung
 String inputString = ""; 
 
 enum Mode { MANUAL, LINE, OBSTACLE };
 Mode currentMode = MANUAL;
+
+// --- SCAN VARIABLEN ---
+int scanAngle = 90;
+int scanDirection = 5; 
+unsigned long lastScanTime = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -30,47 +35,100 @@ void setup() {
   pinMode(ENA, OUTPUT); pinMode(ENB, OUTPUT);
   pinMode(2, INPUT); pinMode(4, INPUT); pinMode(10, INPUT);
   pinMode(TRIG, OUTPUT); pinMode(ECHO, INPUT);
-  
   head.attach(SERVO_PIN);
   head.write(90); 
   stopRobot();
 }
 
 void loop() {
-  // 1. BLUETOOTH DATEN VERARBEITEN
+  // 1. BLUETOOTH PARSER (Verändert carSpeed nur bei Balken-Bewegung)
   while (Serial.available()) {
     char inChar = (char)Serial.read();
-    
-    // Not-Aus bei 's'
-    if (inChar == 's') { 
-      currentMode = MANUAL; 
-      stopRobot(); 
-      inputString = ""; 
-      return; 
-    }
-    
-    // Modus-Wahl (falls App '1' oder '2' separat schickt)
+    if (inChar == 's') { currentMode = MANUAL; stopRobot(); inputString = ""; return; }
     if (inChar == '1' && inputString == "") currentMode = LINE;
-    if (inChar == '2' && inputString == "") currentMode = OBSTACLE;
+    if (inChar == '2' && inputString == "") { currentMode = OBSTACLE; head.write(90); }
 
     inputString += inChar;
-    if (inChar == '}') {
-      parseJoystick(inputString);
-      inputString = "";
-    }
+    if (inChar == '}') { parseJoystick(inputString); inputString = ""; }
   }
 
-  // 2. AKTIVEN MODUS AUSFÜHREN
+  // 2. MODI AUSFÜHREN
   if (currentMode == LINE) {
     runLineTracking();
   } else if (currentMode == OBSTACLE) {
-    runObstacleAvoidance();
+    runSmoothObstacleAvoidance();
   }
 }
 
-// --- PARSER FÜR RICHTUNG (D1) UND GESCHWINDIGKEIT (D2) ---
+// --- OPTIMIERTE SMOOTH OBSTACLE AVOIDANCE (Drosselung nur hier!) ---
+void runSmoothObstacleAvoidance() {
+  // Wir begrenzen die Geschwindigkeit nur INTERN in dieser Funktion auf max. 160
+  // Deine carSpeed am Balken bleibt davon unberührt.
+  int safeBaseSpeed = min(carSpeed, 160); 
+
+  // Servo-Scan
+  if (millis() - lastScanTime > 40) {
+    scanAngle += scanDirection;
+    if (scanAngle >= 140 || scanAngle <= 40) scanDirection = -scanDirection;
+    head.write(scanAngle);
+    lastScanTime = millis();
+  }
+
+  int dist = getDistance();
+
+  if (dist > 0 && dist < 20) {
+    stopRobot();
+    delay(100);
+    // Rückwärts mit dem eingestellten carSpeed
+    back(); delay(400);
+    if (scanAngle > 90) spinRight(); else spinLeft();
+    delay(400);
+    stopRobot();
+  } 
+  else if (dist > 0 && dist < 60) {
+    // Dynamische Geschwindigkeitsanpassung basierend auf dem sicheren Limit
+    int curveSpeed = map(dist, 20, 60, 70, safeBaseSpeed);
+    
+    if (scanAngle > 105) steer(safeBaseSpeed, curveSpeed);      // Ausweichen rechts
+    else if (scanAngle < 75) steer(curveSpeed, safeBaseSpeed);  // Ausweichen links
+    else steer(safeBaseSpeed / 2, safeBaseSpeed / 2);           // Bremsen
+  } 
+  else {
+    steer(safeBaseSpeed, safeBaseSpeed); // Fahrt mit sicherem Tempo
+  }
+}
+
+// --- MOTOR & STEER LOGIK ---
+void steer(int sL, int sR) {
+  analogWrite(ENA, sL); analogWrite(ENB, sR);
+  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);  digitalWrite(IN4, HIGH);
+}
+
+void forward() { steer(carSpeed, carSpeed); }
+
+void back() {
+  analogWrite(ENA, carSpeed); analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+}
+
+void stopRobot() { digitalWrite(ENA, LOW); digitalWrite(ENB, LOW); }
+
+// --- LINIENVERFOLGUNG (Nutzt vollen carSpeed vom Balken) ---
+void runLineTracking() {
+  int speedH = carSpeed; 
+  int speedL = carSpeed / 4; 
+  if (LT_M && !LT_L && !LT_R) forward(); 
+  else if (LT_M && LT_R) steer(speedH, speedL); 
+  else if (LT_M && LT_L) steer(speedL, speedH); 
+  else if (LT_R) spinRight(); 
+  else if (LT_L) spinLeft();
+  else stopRobot();
+}
+
+// --- JOYSTICK PARSER (Aktualisiert carSpeed permanent) ---
 void parseJoystick(String data) {
-  // D1 auslesen (Richtung)
   int d1Index = data.indexOf("\"D1\"");
   if (d1Index != -1) {
     int colonIndex = data.indexOf(":", d1Index);
@@ -80,8 +138,6 @@ void parseJoystick(String data) {
       if (i < data.length()) executeMovement(data[i]);
     }
   }
-
-  // D2 auslesen (Speed-Balken) - NEU!
   int d2Index = data.indexOf("\"D2\"");
   if (d2Index != -1) {
     int colonIndex = data.indexOf(":", d2Index);
@@ -91,9 +147,7 @@ void parseJoystick(String data) {
         if (isDigit(data[i])) speedVal += data[i];
         else if (data[i] == ',' || data[i] == '}' || data[i] == '\n') break;
       }
-      if (speedVal.length() > 0) {
-        carSpeed = speedVal.toInt(); // Übernimmt 0-255 von der App
-      }
+      if (speedVal.length() > 0) carSpeed = speedVal.toInt();
     }
   }
 }
@@ -101,98 +155,48 @@ void parseJoystick(String data) {
 void executeMovement(char dir) {
   currentMode = MANUAL; 
   switch (dir) {
-    case '3': forward(); break;         // OBEN
-    case '4': back();    break;         // UNTEN
-    case '6': forwardLeft(); break;     // OBEN LINKS
-    case '8': forwardRight(); break;    // OBEN RECHTS
-    case '9': backRight(); break;       // UNTEN RECHTS
-    case '7': backLeft(); break;        // UNTEN LINKS
-    case '1': spinLeft(); break;        // LINKS DREHEN
-    case '2': spinRight(); break;       // RECHTS DREHEN
+    case '3': forward(); break;
+    case '4': back(); break;
+    case '6': steer(carSpeed/3, carSpeed); break;
+    case '8': steer(carSpeed, carSpeed/3); break;
+    case '9': backRight(); break;
+    case '7': backLeft(); break;
+    case '1': spinLeft(); break;
+    case '2': spinRight(); break;
     case '0': stopRobot(); break;
   }
 }
 
-// --- MOTOR FUNKTIONEN ---
-void forward() {
-  analogWrite(ENA, carSpeed); analogWrite(ENB, carSpeed);
-  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);  digitalWrite(IN4, HIGH);
-}
-void back() {
-  analogWrite(ENA, carSpeed); analogWrite(ENB, carSpeed);
-  digitalWrite(IN1, LOW);  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
-}
-void forwardLeft() {
-  analogWrite(ENA, carSpeed/3); analogWrite(ENB, carSpeed);
-  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);  digitalWrite(IN4, HIGH);
-}
-void forwardRight() {
-  analogWrite(ENA, carSpeed); analogWrite(ENB, carSpeed/3);
-  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);  digitalWrite(IN4, HIGH);
-}
 void backLeft() {
   analogWrite(ENA, carSpeed/3); analogWrite(ENB, carSpeed);
-  digitalWrite(IN1, LOW);  digitalWrite(IN2, HIGH);
+  digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
 }
+
 void backRight() {
   analogWrite(ENA, carSpeed); analogWrite(ENB, carSpeed/3);
-  digitalWrite(IN1, LOW);  digitalWrite(IN2, HIGH);
+  digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
 }
+
 void spinLeft() {
   analogWrite(ENA, carSpeed); analogWrite(ENB, carSpeed);
-  digitalWrite(IN1, LOW);  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);  digitalWrite(IN4, HIGH);
+  digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH);
 }
+
 void spinRight() {
   analogWrite(ENA, carSpeed); analogWrite(ENB, carSpeed);
   digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
 }
-void stopRobot() {
-  digitalWrite(ENA, LOW); digitalWrite(ENB, LOW);
-}
 
-// --- SMOOTHE LINIENVERFOLGUNG ---
-void runLineTracking() {
-  int speedH = carSpeed; 
-  int speedL = carSpeed / 4; // Dynamisches Lenkverhältnis
-
-  if (LT_M && !LT_L && !LT_R) forward(); 
-  else if (LT_M && LT_R) steer(speedH, speedL); 
-  else if (LT_M && LT_L) steer(speedL, speedH); 
-  else if (LT_R) spinRight(); 
-  else if (LT_L) spinLeft();
-  else stopRobot();
-}
-
-void steer(int sL, int sR) {
-  analogWrite(ENA, sL); analogWrite(ENB, sR);
-  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);  digitalWrite(IN4, HIGH);
-}
-
-// --- HINDERNISVERMEIDUNG ---
-void runObstacleAvoidance() {
-  head.write(90);
+int getDistance() {
   digitalWrite(TRIG, LOW); delayMicroseconds(2);
   digitalWrite(TRIG, HIGH); delayMicroseconds(10);
   digitalWrite(TRIG, LOW);
-  long duration = pulseIn(ECHO, HIGH, 30000);
-  int dist = duration * 0.034 / 2;
-  
-  if (dist > 0 && dist < 25) {
-    stopRobot();
-    head.write(160); delay(400); 
-    head.write(20); delay(400);
-    head.write(90);
-    spinRight(); delay(600); stopRobot();
-  } else {
-    forward();
-  }
+  long duration = pulseIn(ECHO, HIGH, 25000);
+  if (duration == 0) return 400; 
+  return duration * 0.034 / 2;
 }
+
